b55a8dc04cc7bbab50174105b301c5b38990eb7a
CHOIJIHO <wlghsp@gmail.com>
2022-09-01 22:43:29 +0900

    C++ 강의 기초 문제 학습

---
+++ inflearn/2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a, b, i, sum = 0;
+
+    cin>>a>>b;
+    for (i = a; i < b; i++)
+    {
+        cout<<i<<" + "; 
+        sum += i;
+    }
+    sum += i;
+    cout<<i<<" = "<<sum;
+
+}

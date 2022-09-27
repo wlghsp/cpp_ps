a58e4264576a7ef59ebf6d6199f4a727029e0d59
CHOIJIHO <wlghsp@gmail.com>
2022-09-28 08:37:11 +0900

    12738 가장 긴 부분수열 3

---
+++ baekjoon/boj12738/main.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+int main() 
+{
+
+    int N;
+    cin >> N;
+
+    int* data = new int[N];
+
+    for (int i = 0; i < N; i++)
+    {
+        cin >> data[i]; 
+    }
+
+    
+    
+
+
+    return 0;
+
+}

115f49d97674488f71d703a1beaf5431f3f56847
CHOIJIHO <wlghsp@gmail.com>
2023-04-15 22:11:55 +0900

    괄호 쌍 만들어주기 2

---
+++ cpp_study/code_tree/novice_mid/BruteForce1/part1/13.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#define MAX_N 100
+using namespace std;
+
+/*
+괄호 쌍 만들어주기 2
+내 풀이
+*/
+string a;
+
+
+int main() {
+  freopen("input.txt", "rt", stdin);
+  cin >> a;
+  int size = a.size();
+  int ans = 0;
+  for(int i = 0; i < size - 1; i++) {
+      for(int j = i + 1; j < size - 1; j++){
+        if(a[i] == '(' && a[i+1] == '(' && a[j] == ')' && a[j + 1] == ')') {
+          ans++;
+        }
+      }
+  }
+  cout << ans;
+  return 0;
+}
+}

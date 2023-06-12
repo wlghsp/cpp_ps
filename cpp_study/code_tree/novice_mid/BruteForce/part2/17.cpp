0a6a953260d11c31bbe13b768f1f628cd62f03b6
CHOIJIHO <wlghsp@gmail.com>
2023-06-12 22:48:40 +0900

    스승의 은혜 3 내 풀이

--- cpp_study/code_tree/novice_mid/BruteForce/part2/17.cpp
+++ cpp_study/code_tree/novice_mid/BruteForce/part2/17.cpp
@@ -1,23 +1,18 @@
 #include <iostream>
 #include <algorithm>
 #include <climits>
+#include <vector>
+ #include <tuple>
 #define MAX_N 1000
 #define MAX_BPS 1000
 using namespace std;
 /*
 스승의 은혜 3
 내 풀이
-
-N명의 학생에게 B만큼의 예산으로 선물을 주려고 함
-
-학생 i가 원하는 선물의 가격 P(i)과 배송비 S(i)
-
-선물 하나 반값 할인 쿠폰 있땅
-
 */
 
 int N, B; // 학생 수 N, 예산 B
-int arr[MAX_N][2]; 
+vector<tuple<int, int, int>> v; 
 
 void Input() {
   cin >> N >> B;
@@ -25,19 +20,16 @@ void Input() {
   {
     int p, s; // p 선물 가격, 배송비 s
     cin >> p >> s;
-    arr[i][0] = p + s;
-    arr[i][1] = p;
-    arr[i][2] = s;
+    v.push_back(make_tuple(p+s, p, s));
   }
 }
 
-bool compare(const int* a, const int* b) {
-  return a[0] < b[0];
+bool compare(const tuple<int, int, int>& a, const tuple<int, int, int>& b) {
+    return get<0>(a) < get<0>(b);
 }
-
-
 int Search() {
   // 1. 가격과 배송비로 정렬
+  sort(v.begin(), v.end(), compare);
 
   int max_students = 0;
   for (int i = 0; i < N; i++)
@@ -46,7 +38,7 @@ int Search() {
     int students = 0;
     for (int j = 0; j < N; j++)
     {
-      int price = (i == j) ? arr[j][1] / 2:  arr[j][1];
+      int price = (i == j) ? get<1>(v[j]) / 2 + get<2>(v[j]):  get<1>(v[j]) + get<2>(v[j]);
       if ((sum + price) <= B) {
         sum += price;
         students++;

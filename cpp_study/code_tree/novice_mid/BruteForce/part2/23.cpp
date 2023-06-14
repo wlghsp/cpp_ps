e6af574414e9f21fef095125ff437ff84e58dd91
CHOIJIHO <wlghsp@gmail.com>
2023-06-14 19:30:41 +0900

    정보에 따른 숫자 2 풀이 예정

---
+++ cpp_study/code_tree/novice_mid/BruteForce/part2/23.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#define MAX_T 100
+#define MAX_AB 1000
+
+using namespace std;
+/*
+정보에 따른 숫자 2
+내 풀이
+
+1차 수직선 상에 T개의 알파벳이 놓임
+
+
+*/
+
+int T, a, b;
+
+void Input() {
+  cin >> T >> a >> b;
+}
+
+
+int Search() {
+  int cnt = 0;
+
+  return cnt;
+}
+
+
+int main() {
+  freopen("input.txt", "rt", stdin);
+  Input();
+  cout << Search();
+  return 0;
+}

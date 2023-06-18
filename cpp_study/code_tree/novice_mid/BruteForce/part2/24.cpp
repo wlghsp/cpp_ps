0b7c380b9d9d8258ee6a0a3c1f86fbe459433283
JIHO CHOI <wlghsp@gmail.com>
2023-06-18 22:53:27 +0900

    등차수열 풀이 예정

---
+++ cpp_study/code_tree/novice_mid/BruteForce/part2/24.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <climits>
+#define MAX_N 100
+using namespace std;
+/*
+등차수열
+내 풀이
+*/
+
+int n;
+int arr[MAX_N + 1];
+
+void Input() {
+  cin >> n;
+  for (int i = 0; i < n; i++)
+  {
+    cin >> arr[i];
+  }
+}
+
+int Search() {
+  int cnt = 0;
+  
+  return cnt;
+}
+
+
+int main() {
+  freopen("input.txt", "rt", stdin);
+  Input();
+  cout << Search();
+  return 0;
+}

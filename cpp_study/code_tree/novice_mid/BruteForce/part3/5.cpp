955eb77b60498bd6e223b73c077a01d2eb9965e4
JIHO CHOI <wlghsp@gmail.com>
2023-06-21 23:19:57 +0900

    수를 여러번 사용하여 특정 수 만들기 풀이 예정

---
+++ cpp_study/code_tree/novice_mid/BruteForce/part3/5.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+#include <vector>
+#define MAX_N 100
+using namespace std;
+/*
+수를 여러번 사용하여 특정 수 만들기
+내 풀이
+*/
+
+int a, b, c;
+
+void Input() {
+  cin >> a >> b >> c;
+}
+
+int Search() {
+  int max_sum = INT_MIN;
+  return max_sum;
+}
+
+
+int main() {
+  freopen("input.txt", "rt", stdin);
+  Input();
+  cout << Search();
+  return 0;
+}
